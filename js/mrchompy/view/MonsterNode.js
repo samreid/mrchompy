41f377f67ca5ba8d3f684aecba547bdddd4bbf7f
Sam Reid <reids@colorado.edu>
2015-08-22 16:43:50 -0600

    started teeth

--- js/mrchompy/view/MonsterNode.js
+++ js/mrchompy/view/MonsterNode.js
@@ -1,144 +1,91 @@
+//  Copyright 2002-2014, University of Colorado Boulder
+
+/**
+ *
+ *
+ * @author Sam Reid (PhET Interactive Simulations)
+ */
 define( function( require ) {
   'use strict';
 
   // modules
   var inherit = require( 'PHET_CORE/inherit' );
-  var WebGLNode = require( 'SCENERY/nodes/WebGLNode' );
-  var Bounds2 = require( 'DOT/Bounds2' );
-  var ShaderProgram = require( 'SCENERY/util/ShaderProgram' );
-  var Vector2 = require( 'DOT/Vector2' );
+  var Node = require( 'SCENERY/nodes/Node' );
+  var Rectangle = require( 'SCENERY/nodes/Rectangle' );
+  var Shape = require( 'KITE/Shape' );
+  var Path = require( 'SCENERY/nodes/Path' );
 
   /**
-   * @param {ModelViewTransform2} modelViewTransform - converts between model and view co-ordinates
-   * @param {number} stageWidth - width of the dev area
-   * @param {number} stageHeight - height of the dev area
-   * @param {} triangles -
+   *
    * @constructor
    */
-  function MonsterNode( stageWidth, stageHeight, triangles, offset ) {
+  function MonsterNode() {
+    Node.call( this );
 
-    WebGLNode.call( this, {
-      canvasBounds: new Bounds2( 0, 0, stageWidth, stageHeight )
-    } );
-    this.stageHeight = stageHeight; // @private
-    this.stageWidth = stageWidth; // @private
+    var t = Date.now();
+    var maxAlpha1 = Math.PI / 5;
+    var minAlpha1 = 0;
+    var maxAlpha2 = Math.PI / 8;
+    var minAlpha2 = 0;
+    var monsterNode = this;
 
-    this.triangles = triangles;
-    this.offset = offset;
-    this.invalidatePaint();
+    // test with oscillation
+    var omega = t / 1000 * 5;
+    var alpha1 = Math.abs( Math.sin( omega ) * (maxAlpha1 - minAlpha1) ) + minAlpha1;
+    //var alpha1 = Math.PI / 5;
 
-    this.strokeWidth = 10;
+    var alpha2 = Math.abs( Math.sin( omega ) * (maxAlpha2 - minAlpha2) ) + minAlpha2;
+    //var alpha2 = Math.PI / 8;
+
+    //this.addChild( new Rectangle( 0, 0, 100, 100, { fill: 'red' } ) );
+    //var jawAngle = Math.PI / 4;
+
+    var j1 = 300;
+    var j2 = 280;
+
+    var jawPoint1X = j1 * Math.cos( alpha1 );
+    var jawPoint1Y = j1 * Math.sin( alpha1 );
+
+    var jawPoint2X = j2 * Math.cos( alpha2 );
+    var jawPoint2Y = -j2 * Math.sin( alpha2 );
+
+    var shape = new Shape();
+    shape.moveTo( 0, 0 );
+    shape.lineTo( jawPoint1X, jawPoint1Y );
+    shape.lineTo( -100, 0 );
+    shape.lineTo( jawPoint2X, jawPoint2Y );
+    shape.close();
+
+    var toRadians = function( degrees ) {
+      return degrees / 180 * Math.PI;
+    };
+    var addLowerTooth = function( distanceFromFrontOfJawToTooth, toothBaseLength, toothHeight, toothAngle ) {
+      var tooth1 = new Shape();
+      var toothPoint1x = (j1 - distanceFromFrontOfJawToTooth) * Math.cos( alpha1 );
+      var toothPoint1y = (j1 - distanceFromFrontOfJawToTooth) * Math.sin( alpha1 );
+      tooth1.moveTo( toothPoint1x, toothPoint1y );
+      tooth1.lineTo(
+        toothPoint1x - toothHeight * Math.cos( alpha1 + toothAngle ),
+        toothPoint1y - toothHeight * Math.sin( alpha1 + toothAngle )
+      );
+      var toothPoint2x = (j1 - distanceFromFrontOfJawToTooth - toothBaseLength) * Math.cos( alpha1 );
+      var toothPoint2y = (j1 - distanceFromFrontOfJawToTooth - toothBaseLength) * Math.sin( alpha1 );
+      tooth1.lineTo( toothPoint2x, toothPoint2y );
+      tooth1.close();
+      monsterNode.addChild( new Path( tooth1, { fill: 'white', stroke: 'gray', lineWidth: 2 } ) );
+    };
+    addLowerTooth( 0, 30, 80, toRadians( 60 ) );
+    addLowerTooth( 25, 30, 70, toRadians( 70 ) );
+    addLowerTooth( 38, 30, 70, toRadians( 65 ) );
+    addLowerTooth( 54, 30, 70, toRadians( 60 ) );
+    addLowerTooth( 60, 30, 50, toRadians( 50 ) );
+    addLowerTooth( 80, 30, 60, toRadians( 60 ) );
+    addLowerTooth( 100, 30, 70, toRadians( 60 ) );
+    addLowerTooth( 140, 30, 70, toRadians( 70 ) );
+    this.addChild( new Path( shape, { fill: 'green', stroke: 'gray' } ) );
+
+
   }
 
-  return inherit( WebGLNode, MonsterNode, {
-
-    initializeWebGLDrawable: function( drawable ) {
-      var gl = drawable.gl;
-
-      // Simple example for custom shader
-      var vertexShaderSource = [
-        // Position
-        'attribute vec3 aPosition;',
-        'attribute vec4 aColor;',
-        'varying vec4 vColor;',
-        'uniform mat3 uModelViewMatrix;',
-        'uniform mat3 uProjectionMatrix;',
-
-        'void main( void ) {',
-        '  vColor = aColor;',
-
-        // homogeneous model-view transformation
-        '  vec3 view = uModelViewMatrix * vec3( aPosition.xy, 1 );',
-
-        // homogeneous map to to normalized device coordinates
-        '  vec3 ndc = uProjectionMatrix * vec3( view.xy, 1 );',
-
-        // combine with the z coordinate specified
-        '  gl_Position = vec4( ndc.xy, aPosition.z, 1.0 );',
-        '}'
-      ].join( '\n' );
-
-      // Simple demo for custom shader
-      var fragmentShaderSource = [
-        'precision mediump float;',
-        'varying vec4 vColor;',
-
-        // Returns the color from the vertex shader
-        'void main( void ) {',
-        '  gl_FragColor = vColor;',
-        '}'
-      ].join( '\n' );
-
-      drawable.shaderProgram = new ShaderProgram( gl, vertexShaderSource, fragmentShaderSource, {
-        attributes: [ 'aPosition', 'aColor' ],
-        uniforms: [ 'uModelViewMatrix', 'uProjectionMatrix' ]
-      } );
-
-      drawable.vertexBuffer = gl.createBuffer();
-      drawable.colorBuffer = gl.createBuffer();
-
-      this.updateBuffers( drawable );
-    },
-    updateBuffers: function( drawable ) {
-      var gl = drawable.gl;
-
-      var points = [];
-      var colors = [];
-
-      for ( var i = 0; i < this.triangles.length; i++ ) {
-        var triangle = this.triangles[ i ];
-
-        points.push(
-          triangle.x1 + this.offset.x, triangle.y1 + this.offset.y, 0.2,
-          triangle.x2 + this.offset.x, triangle.y2 + this.offset.y, 0.2,
-          triangle.x3 + this.offset.x, triangle.y3 + this.offset.y, 0.2
-        );
-
-        colors.push(
-          triangle.r, triangle.g, triangle.b, triangle.a,
-          triangle.r, triangle.g, triangle.b, triangle.a,
-          triangle.r, triangle.g, triangle.b, triangle.a
-        );
-      }
-
-      gl.bindBuffer( gl.ARRAY_BUFFER, drawable.vertexBuffer );
-      gl.bufferData( gl.ARRAY_BUFFER, new Float32Array( points ), gl.STATIC_DRAW );
-
-      gl.bindBuffer( gl.ARRAY_BUFFER, drawable.colorBuffer );
-      gl.bufferData( gl.ARRAY_BUFFER, new Float32Array( colors ), gl.STATIC_DRAW );
-    },
-
-    paintWebGLDrawable: function( drawable, matrix ) {
-      this.updateBuffers( drawable );
-
-      var gl = drawable.gl;
-      var shaderProgram = drawable.shaderProgram;
-
-      shaderProgram.use();
-
-      gl.uniformMatrix3fv( shaderProgram.uniformLocations.uModelViewMatrix, false, matrix.entries );
-      gl.uniformMatrix3fv( shaderProgram.uniformLocations.uProjectionMatrix, false, drawable.webGLBlock.projectionMatrixArray );
-
-      gl.bindBuffer( gl.ARRAY_BUFFER, drawable.vertexBuffer );
-      gl.vertexAttribPointer( shaderProgram.attributeLocations.aPosition, 3, gl.FLOAT, false, 0, 0 );
-
-      gl.bindBuffer( gl.ARRAY_BUFFER, drawable.colorBuffer );
-      gl.vertexAttribPointer( shaderProgram.attributeLocations.aColor, 4, gl.FLOAT, false, 0, 0 );
-
-      // 2 triangles per ray
-      gl.drawArrays( gl.TRIANGLES, 0, this.triangles.length * 3 );
-
-      shaderProgram.unuse();
-    },
-
-    disposeWebGLDrawable: function( drawable ) {
-      drawable.shaderProgram.dispose();
-      drawable.gl.deleteBuffer( drawable.vertexBuffer );
-
-      drawable.shaderProgram = null;
-    },
-    step: function() {
-      this.invalidatePaint();
-    }
-  } );
+  return inherit( Node, MonsterNode );
 } );
---
+++ js/mrchompy/view/MonsterNodeWebGL.js
@@ -0,0 +1,144 @@
+define( function( require ) {
+  'use strict';
+
+  // modules
+  var inherit = require( 'PHET_CORE/inherit' );
+  var WebGLNode = require( 'SCENERY/nodes/WebGLNode' );
+  var Bounds2 = require( 'DOT/Bounds2' );
+  var ShaderProgram = require( 'SCENERY/util/ShaderProgram' );
+  var Vector2 = require( 'DOT/Vector2' );
+
+  /**
+   * @param {ModelViewTransform2} modelViewTransform - converts between model and view co-ordinates
+   * @param {number} stageWidth - width of the dev area
+   * @param {number} stageHeight - height of the dev area
+   * @param {} triangles -
+   * @constructor
+   */
+  function MonsterNode( stageWidth, stageHeight, triangles, offset ) {
+
+    WebGLNode.call( this, {
+      canvasBounds: new Bounds2( 0, 0, stageWidth, stageHeight )
+    } );
+    this.stageHeight = stageHeight; // @private
+    this.stageWidth = stageWidth; // @private
+
+    this.triangles = triangles;
+    this.offset = offset;
+    this.invalidatePaint();
+
+    this.strokeWidth = 10;
+  }
+
+  return inherit( WebGLNode, MonsterNode, {
+
+    initializeWebGLDrawable: function( drawable ) {
+      var gl = drawable.gl;
+
+      // Simple example for custom shader
+      var vertexShaderSource = [
+        // Position
+        'attribute vec3 aPosition;',
+        'attribute vec4 aColor;',
+        'varying vec4 vColor;',
+        'uniform mat3 uModelViewMatrix;',
+        'uniform mat3 uProjectionMatrix;',
+
+        'void main( void ) {',
+        '  vColor = aColor;',
+
+        // homogeneous model-view transformation
+        '  vec3 view = uModelViewMatrix * vec3( aPosition.xy, 1 );',
+
+        // homogeneous map to to normalized device coordinates
+        '  vec3 ndc = uProjectionMatrix * vec3( view.xy, 1 );',
+
+        // combine with the z coordinate specified
+        '  gl_Position = vec4( ndc.xy, aPosition.z, 1.0 );',
+        '}'
+      ].join( '\n' );
+
+      // Simple demo for custom shader
+      var fragmentShaderSource = [
+        'precision mediump float;',
+        'varying vec4 vColor;',
+
+        // Returns the color from the vertex shader
+        'void main( void ) {',
+        '  gl_FragColor = vColor;',
+        '}'
+      ].join( '\n' );
+
+      drawable.shaderProgram = new ShaderProgram( gl, vertexShaderSource, fragmentShaderSource, {
+        attributes: [ 'aPosition', 'aColor' ],
+        uniforms: [ 'uModelViewMatrix', 'uProjectionMatrix' ]
+      } );
+
+      drawable.vertexBuffer = gl.createBuffer();
+      drawable.colorBuffer = gl.createBuffer();
+
+      this.updateBuffers( drawable );
+    },
+    updateBuffers: function( drawable ) {
+      var gl = drawable.gl;
+
+      var points = [];
+      var colors = [];
+
+      for ( var i = 0; i < this.triangles.length; i++ ) {
+        var triangle = this.triangles[ i ];
+
+        points.push(
+          triangle.x1 + this.offset.x, triangle.y1 + this.offset.y, 0.2,
+          triangle.x2 + this.offset.x, triangle.y2 + this.offset.y, 0.2,
+          triangle.x3 + this.offset.x, triangle.y3 + this.offset.y, 0.2
+        );
+
+        colors.push(
+          triangle.r, triangle.g, triangle.b, triangle.a,
+          triangle.r, triangle.g, triangle.b, triangle.a,
+          triangle.r, triangle.g, triangle.b, triangle.a
+        );
+      }
+
+      gl.bindBuffer( gl.ARRAY_BUFFER, drawable.vertexBuffer );
+      gl.bufferData( gl.ARRAY_BUFFER, new Float32Array( points ), gl.STATIC_DRAW );
+
+      gl.bindBuffer( gl.ARRAY_BUFFER, drawable.colorBuffer );
+      gl.bufferData( gl.ARRAY_BUFFER, new Float32Array( colors ), gl.STATIC_DRAW );
+    },
+
+    paintWebGLDrawable: function( drawable, matrix ) {
+      this.updateBuffers( drawable );
+
+      var gl = drawable.gl;
+      var shaderProgram = drawable.shaderProgram;
+
+      shaderProgram.use();
+
+      gl.uniformMatrix3fv( shaderProgram.uniformLocations.uModelViewMatrix, false, matrix.entries );
+      gl.uniformMatrix3fv( shaderProgram.uniformLocations.uProjectionMatrix, false, drawable.webGLBlock.projectionMatrixArray );
+
+      gl.bindBuffer( gl.ARRAY_BUFFER, drawable.vertexBuffer );
+      gl.vertexAttribPointer( shaderProgram.attributeLocations.aPosition, 3, gl.FLOAT, false, 0, 0 );
+
+      gl.bindBuffer( gl.ARRAY_BUFFER, drawable.colorBuffer );
+      gl.vertexAttribPointer( shaderProgram.attributeLocations.aColor, 4, gl.FLOAT, false, 0, 0 );
+
+      // 2 triangles per ray
+      gl.drawArrays( gl.TRIANGLES, 0, this.triangles.length * 3 );
+
+      shaderProgram.unuse();
+    },
+
+    disposeWebGLDrawable: function( drawable ) {
+      drawable.shaderProgram.dispose();
+      drawable.gl.deleteBuffer( drawable.vertexBuffer );
+
+      drawable.shaderProgram = null;
+    },
+    step: function() {
+      this.invalidatePaint();
+    }
+  } );
+} );
