c8ce24054037c422fa80aa0cef5ed27511f27b85
Sam Reid <reids@colorado.edu>
2015-08-23 08:16:31 -0600

    added hooks for keys

--- js/mrchompy/view/MrchompyScreenView.js
+++ js/mrchompy/view/MrchompyScreenView.js
@@ -31,6 +31,18 @@ define( function( require ) {
     this.addChild( monsterNode );
 
     this.step = function() {
+
+      if ( pressedKeys[ 39 ] ) {
+
+        // right
+        console.log( 'right' );
+      }
+      if ( pressedKeys[ 37 ] ) {
+        console.log( 'left' );
+      }
+      if ( pressedKeys[ 38 ] ) {
+        console.log( 'up' );
+      }
       this.removeChild( monsterNode );
 
       monsterNode = new MonsterNode();
@@ -38,6 +50,36 @@ define( function( require ) {
       monsterNode.bottom = ground.top;
       this.addChild( monsterNode );
     };
+
+    // Key listeners 
+    // http://stackoverflow.com/questions/16089421/simplest-way-to-detect-keypresses-in-javascript
+    function addEvent( element, eventName, callback ) {
+      if ( element.addEventListener ) {
+        element.addEventListener( eventName, callback, false );
+      }
+      else if ( element.attachEvent ) {
+        element.attachEvent( "on" + eventName, callback );
+      }
+      else {
+        element[ "on" + eventName ] = callback;
+      }
+    }
+
+    var pressedKeys = {};
+    addEvent( document, "keydown", function( e ) {
+      var e = e || window.event;
+      // use e.keyCode
+      console.log( e.keyCode, 'down' );
+      pressedKeys[ e.keyCode ] = true;
+    } );
+
+    addEvent( document, "keyup", function( e ) {
+      var e = e || window.event;
+      // use e.keyCode
+      console.log( e.keyCode, 'up' );
+      pressedKeys[ e.keyCode ] = false;
+    } );
+
   }
 
   return inherit( ScreenView, MrchompyScreenView );
